a7550e22d1c9445153f4987d14c47595959ad810
ekasaputra0324 <paijomr4221@gmail.com>
2023-02-15 04:58:56 +0000

    learning components react js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import navbar from "./Navbar";
+import Welcome from "./Welcome";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// create render function  
+const render = (content , id) =>{
+  ReactDOM.render(content, document.getElementById(id));
+}
+
+
+// call render function
+render(navbar, 'nav')
+render(<Welcome name="eka"  />, 'root')
+
